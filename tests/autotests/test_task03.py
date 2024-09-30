fbf40f3fc306c2a8a37228f571efde31737acc57
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-09-30 23:12:26 +0300

    refactor: Move all rpq function to rpq.py file

--- tests/autotests/test_task03.py
+++ tests/autotests/test_task03.py
@@ -16,8 +16,8 @@ try:
     from project.finite_automata import (
         AdjacencyMatrixFA,
         intersect_automata,
-        tensor_based_rpq,
     )
+    from project.rpq import tensor_based_rpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 3 is not ready to test!")
 
